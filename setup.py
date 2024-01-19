c554d1aa980effc289a7689eda18f0fa4689108b
iffishells <Iftikhar@revolveai.com>
2024-01-19 12:06:22 +0500

    added initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project about to summarize the English content',
+    author='iffishells',
+    license='MIT',
+)
